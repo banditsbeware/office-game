4b4b525c4aea2eff9261d941ff5dd4b829f7292b
ShaneVincent <gamegamesgamers@gmail.com>
2022-10-05 13:54:49 -0700

    camera clamp

--- Assets/Scripts/MainPlayer&Camera/FollowMan.cs
+++ Assets/Scripts/MainPlayer&Camera/FollowMan.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FollowMan : MonoBehaviour
 {
+    private Vector3 xyzMin = new Vector3(0, -5, -10);
+    private Vector3 xyzMax = new Vector3(9, 8, -10);
+
+
     public Transform player;
     public Vector3 offset;
     public float smoothSpeed = .125f;
@@ -12,6 +16,35 @@ public class FollowMan : MonoBehaviour
     {
         Vector3 desiredPos = player.transform.position + offset;
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
-        transform.position = smoothedPos;
+        transform.position = Clamp(smoothedPos, xyzMin, xyzMax);
+    }
+
+    // public static int Clamp(int value, int min, int max)
+    // {
+    //     return (value < min) ? min : (value > max) ? max : value;
+    // }
+
+    public static Vector3 Clamp(Vector3 vector, Vector3 min, Vector3 max)
+    {
+        Vector3 clampd = new Vector3(0, 0, 0);
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if (vector[i] <= min[i])
+            {
+                clampd[i] = min[i];
+            } 
+            else if (vector[i] >= max[i])
+            {
+                clampd[i] = max[i];
+            } 
+            else 
+            {
+                clampd[i] = vector[i];
+            }
+        }
+
+        return  clampd;
     }
 }
+
