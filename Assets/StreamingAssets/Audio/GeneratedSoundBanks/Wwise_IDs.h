048a63af8539bc8362807a10b1b3612f02b865e3
ShaneVincent <gamegamesgamers@gmail.com>
2022-10-13 22:08:09 -0700

    add Harold dialogue

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,37 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ACTOR = 3231912389U;
+        static const AkUniqueID PLAY_BOSS = 1693071901U;
+        static const AkUniqueID PLAY_CHILD = 2952268476U;
+        static const AkUniqueID PLAY_CLIENT = 2874228519U;
+        static const AkUniqueID PLAY_CLOTHES = 3037538514U;
+        static const AkUniqueID PLAY_COFFEE = 1012391550U;
+        static const AkUniqueID PLAY_EXTRA_COUGH = 393588247U;
+        static const AkUniqueID PLAY_EXTRA_HUH = 1498563514U;
+        static const AkUniqueID PLAY_GAME = 216639764U;
+        static const AkUniqueID PLAY_GOODBYE_ADIOS = 1256120352U;
+        static const AkUniqueID PLAY_GOODBYE_ALRIGHTY = 4060144966U;
+        static const AkUniqueID PLAY_GOODBYE_SEEYA = 363572393U;
+        static const AkUniqueID PLAY_GOODBYE_TILLNEXT = 3414812528U;
+        static const AkUniqueID PLAY_GROWTH = 3799175311U;
         static const AkUniqueID PLAY_HELVETICA = 3069382355U;
+        static const AkUniqueID PLAY_HOURS = 1710785415U;
+        static const AkUniqueID PLAY_INTERNET = 3049503277U;
+        static const AkUniqueID PLAY_MERGER = 2866310100U;
+        static const AkUniqueID PLAY_MOVIES = 1667103285U;
         static const AkUniqueID PLAY_OFFICE_AMB = 219189707U;
+        static const AkUniqueID PLAY_PRICES = 3331051860U;
+        static const AkUniqueID PLAY_RELEASE = 692169189U;
+        static const AkUniqueID PLAY_SCHOOL = 3606423206U;
+        static const AkUniqueID PLAY_SCIFI = 3039012154U;
+        static const AkUniqueID PLAY_SNOW = 4026051667U;
+        static const AkUniqueID PLAY_SOCCER = 3545779475U;
+        static const AkUniqueID PLAY_SPORTS = 1629223547U;
+        static const AkUniqueID PLAY_TEAM = 3422393765U;
+        static const AkUniqueID PLAY_VARSITY = 1692086900U;
+        static const AkUniqueID PLAY_WEATHER = 3308884970U;
+        static const AkUniqueID PLAY_WORK = 1221848793U;
         static const AkUniqueID STOP_HELVETICA = 2823449565U;
         static const AkUniqueID STOP_OFFICE_AMB = 1325704673U;
     } // namespace EVENTS
@@ -40,14 +69,16 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CROSSWORD = 630707431U;
         static const AkUniqueID MAIN = 3161908922U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID DIALOGUE_ = 1669749354U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
