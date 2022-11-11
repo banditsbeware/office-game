9234b1d755b72fd37ea8ed49260c09099f02fe13
ShangusBurger <gamegamesgamers@gmail.com>
2022-11-10 23:25:14 -0800

    wall art

--- Assets/Scripts/Minigames/Printer.cs
+++ Assets/Scripts/Minigames/Printer.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public class Printer : MonoBehaviour
 {
-    
+    public TMP_Text numScreen;
     public void keypadInput(int num)
     {
-        Debug.Log(num);
+        if (numScreen.text.Length < 8)
+        {
+            numScreen.text += num;
+        }
+        
+    }
+
+    public void back()
+    {
+        if (numScreen.text.Length > 0)
+        {
+            numScreen.text = numScreen.text.Remove(numScreen.text.Length - 1);
+        }
+    }
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+            {
+                if (Char.IsDigit(c))
+                {
+                    keypadInput((int) Char.GetNumericValue(c));
+                }
+            }
     }
 }
