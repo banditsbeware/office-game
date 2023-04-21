da3508a19dd5c428970a1c118f297b55587d9399
ShangusBurger <gamegamesgamers@gmail.com>
2023-04-20 21:51:40 -0700

    pat and jax, dog boys

--- Assets/Scripts/Audio/dopplerEffect.cs
+++ Assets/Scripts/Audio/dopplerEffect.cs
@@ -9,23 +9,24 @@ public class dopplerEffect : MonoBehaviour
 	public float DopplerFactor = 3.0f;
 	Vector3 emitterLastPosition = Vector3.zero;
 	Vector3 listenerLastPosition = Vector3.zero;
-    GameObject player;
+  GameObject player;
 	
 	private void Start() 
-    {
-        player = GameObject.Find("Player");
-    }
+	{
+		player = GameObject.Find("Player");
+	}
 
-	private void FixedUpdate () {
-		// get velocity of source/emitter manually
+	private void FixedUpdate () 
+	{
+		//get velocity of source/emitter manually
 		Vector3 emitterSpeed = (emitterLastPosition - transform.position) / Time.fixedDeltaTime;
 		emitterLastPosition = transform.position;
 	
-		// get velocity of listener/player manually
+		//get velocity of listener/player manually
 		Vector3 listenerSpeed = (listenerLastPosition - player.transform.position) / Time.fixedDeltaTime;
 		listenerLastPosition = player.transform.position;
 		
-		// do doppler calc - see http://i.imgur.com/h5BMRmr.png or http://redmine.spatdif.org/projects/spatdif/wiki/Doppler_Extension (OpenAL's implementation of doppler)
+		//do doppler calc
 		var distance = (player.transform.position - transform.position); // source to listener vector
 		var listenerRelativeSpeed = Vector3.Dot(distance, listenerSpeed) / distance.magnitude;
 		var emitterRelativeSpeed = Vector3.Dot(distance, emitterSpeed) / distance.magnitude;
@@ -33,7 +34,7 @@ public class dopplerEffect : MonoBehaviour
 		emitterRelativeSpeed = Mathf.Min (emitterRelativeSpeed, (SpeedOfSound / DopplerFactor));
 		var dopplerPitch = (SpeedOfSound + (listenerRelativeSpeed * DopplerFactor)) / (SpeedOfSound + (emitterRelativeSpeed * DopplerFactor));
 
-		// pass the dopplerPitch through to an RTPC in Wwise (or do whatever you want with the value!)
+		// pass dopplerPitch to an RTPC in Wwise
 		AkSoundEngine.SetRTPCValue("dopplerNum", dopplerPitch, gameObject); 
-    }
+  }
 }
